4ba3140ef20b0ba61d08f540c367499bb96dda19
Wade Foster <jwade109@vt.edu>
2017-01-27 21:20:21 -0500

    Finished drive_node.cpp. Should work fine now.

--- bldc_uart_beaglebone/src/drive_node.cpp
+++ bldc_uart_beaglebone/src/drive_node.cpp
@@ -1,43 +1,80 @@
 // drive_node.cpp
+// alternates turning motors on and off for 3 seconds each.
 
 #include <ros/ros.h>
 #include <std_msgs/Int64.h>
+#include <string>
 
 #include "comm_uart.h"
 #include "bldc_interface.h"
 
-int power;
+
+
+// control variables // ! -- SET THESE VALUES BEFORE RUNNING -- ! //
+const int max_erpm = 0; 
+const float duty = 0;
+const float amps = 0;
+const float brake = 0;
+
 
 int main(int argc, char **argv)
 {
+	if (max_erpm == 0)
+	{
+		ROS_WARN_STREAM("Max RPM is 0");
+	}
+
+
 	// initialize the ROS system.
 	ros::init(argc, argv, "drive_node");
 
 	// establish this program as an ROS node.
 	ros::NodeHandle nh;
 
-	ros::Publisher pub = nh.advertise<std_msgs::Int64>("/drive/status", 1000);
-
-	// indicates the node has started
-	ROS_INFO_STREAM("DRIVING");
-
+	// initialize the UART interface
 	comm_uart_init();
 
-	ros::Rate rate(0.5);
+	// set the current in amps
+	bldc_interface_set_current(amps);
+
+	// set the brake current in amps
+	bldc_interface_set_current_brake(brake);
+
+	// set the duty cycle
+	bldc_interface_set_duty_cycle(duty);
+
+
+	// create a topic which will contain motor speed
+	ros::Publisher pub = nh.advertise<std_msgs::Int64>("/drive/rpm", 1000);
+
+	std::stringstream message;
+	message << "DRIVING. Max RPM: " << max_erpm << ", duty cycle: " << duty;
+	message << ", current: " << amps << " amps, brake current: " << brake << " amps.";
+	ROS_INFO_STREAM(message.str());
+
+
+	int current_RPM = 0;
+
+	ros::Rate rate(0.333);
 	while(ros::ok())
 	{
-		if (power == 100)
+		if (current_RPM == max_erpm)
 		{
-			power = 0;
-			ROS_INFO_STREAM("MOTORS OFF");
+			current_RPM = 0;
+			ROS_INFO_STREAM("Motors OFF");
 		}
 		else
 		{
-			power = 100;
-			ROS_INFO_STREAM("MOTORS ON");
+			current_RPM = max_erpm;
+			ROS_INFO_STREAM("Motors ON");
 		}
+
+		// sets motor speed
+		bldc_interface_set_rpm(current_RPM);
+
+		// publish a message containing motor speed
 		std_msgs::Int64 message;
-		message.data = power;
+		message.data = current_RPM;
 		pub.publish(message);
 		rate.sleep();
 	}

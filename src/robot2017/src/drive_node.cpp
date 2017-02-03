1bee34f777ab427ab8d16e42b6c3543ad471dbd7
Anirudh Bagde <anidev.aelico@gmail.com>
2017-02-03 00:38:46 -0500

    Changed tabs to spaces (sorry)

--- src/robot2017/src/drive_node.cpp
+++ src/robot2017/src/drive_node.cpp
@@ -19,75 +19,75 @@ void teleopReceived(const robot2017::Teleop& msg)
 {
     // byte range should be [-100, 100]
     ROS_INFO("Driving left: %d, right: %d", msg.y_l_thumb, msg.y_r_thumb);
-	driveLeft.set(msg.y_l_thumb * 1.0f / 100.0f);
+    driveLeft.set(msg.y_l_thumb * 1.0f / 100.0f);
     driveRight.set(msg.y_r_thumb * 1.0f / 100.0f);
 }
 
 int main(int argc, char **argv)
 {
-	for (int i = 0; i < argc; ++i)
-	{
-		if (strcmp(argv[i], "-pc") == 0)
-		{
-			onBeagleBone = false;
-			ROS_DEBUG_STREAM("Node is not being run on a BeagleBone");
-		}
-	}
+    for (int i = 0; i < argc; ++i)
+    {
+        if (strcmp(argv[i], "-pc") == 0)
+        {
+            onBeagleBone = false;
+            ROS_DEBUG_STREAM("Node is not being run on a BeagleBone");
+        }
+    }
 
 
-	// initialize the ROS system.
-	ros::init(argc, argv, "drive_node");
+    // initialize the ROS system.
+    ros::init(argc, argv, "drive_node");
 
-	// establish this program as an ROS node.
-	ros::NodeHandle nh;
+    // establish this program as an ROS node.
+    ros::NodeHandle nh;
 
-	// initialize the motors interface
-	if (onBeagleBone)
-	{
-		Motor::init();
-	}
+    // initialize the motors interface
+    if (onBeagleBone)
+    {
+        Motor::init();
+    }
 
-	// create a topic which will contain motor speed
-	pub = nh.advertise<std_msgs::Int64>("/robot/rpm", 1000);
+    // create a topic which will contain motor speed
+    pub = nh.advertise<std_msgs::Int64>("/robot/rpm", 1000);
 
-	ros::Subscriber sub = nh.subscribe("/robot/teleop", 1000, &teleopReceived);
+    ros::Subscriber sub = nh.subscribe("/robot/teleop", 1000, &teleopReceived);
 
-// 	std::stringstream message;
-// 	message << "DRIVING. Max RPM: " << max_erpm << ", duty cycle: " << duty;
-// 	message << ", current: " << amps << " amps, brake current: " << brake << " amps.";
-// 	ROS_INFO_STREAM(message.str());
+//     std::stringstream message;
+//     message << "DRIVING. Max RPM: " << max_erpm << ", duty cycle: " << duty;
+//     message << ", current: " << amps << " amps, brake current: " << brake << " amps.";
+//     ROS_INFO_STREAM(message.str());
 
-	int current_RPM = 0;
+    int current_RPM = 0;
 
-	int tick = 0;
+    int tick = 0;
 
     ROS_INFO("Astrobotics 2017 ready");
     ros::spin();
-/*	ros::Rate rate(refreshRate);
-	while(ros::ok())
-	{
-		if (tick % (refreshRate * 6) == 0)
-		{
-			current_RPM = 0;
-			ROS_INFO_STREAM("Motors OFF");
-		}
-		else if (tick % (refreshRate * 3) == 0)
-		{
-			current_RPM = max_erpm;
-			ROS_INFO_STREAM("Motors ON");
-		}
+/*    ros::Rate rate(refreshRate);
+    while(ros::ok())
+    {
+        if (tick % (refreshRate * 6) == 0)
+        {
+            current_RPM = 0;
+            ROS_INFO_STREAM("Motors OFF");
+        }
+        else if (tick % (refreshRate * 3) == 0)
+        {
+            current_RPM = max_erpm;
+            ROS_INFO_STREAM("Motors ON");
+        }
 
-		// sets motor speed
-		// bldc_interface_set_rpm(current_RPM);
+        // sets motor speed
+        // bldc_interface_set_rpm(current_RPM);
 
-		// publish a message containing motor speed
-		std_msgs::Int64 message;
-		message.data = current_RPM;
-		pub.publish(message);
-		rate.sleep();
-		ros::spinOnce();
-		++tick;
-	}*/
+        // publish a message containing motor speed
+        std_msgs::Int64 message;
+        message.data = current_RPM;
+        pub.publish(message);
+        rate.sleep();
+        ros::spinOnce();
+        ++tick;
+    }*/
 
-	return 0;
+    return 0;
 }
--- src/robot2017/src/testController.cpp
+++ src/robot2017/src/testController.cpp
@@ -15,39 +15,39 @@ bool randomBool()
 
 int main(int argc, char **argv)
 {
-	ros::init(argc, argv, "testController");
+    ros::init(argc, argv, "testController");
 
-	ros::NodeHandle nh;
+    ros::NodeHandle nh;
 
-	// create a topic which will contain motor speed
-	ros::Publisher teleopPub = nh.advertise<robot2017::Teleop>("/robot/teleop", 1000);
+    // create a topic which will contain motor speed
+    ros::Publisher teleopPub = nh.advertise<robot2017::Teleop>("/robot/teleop", 1000);
 
-	ros::Rate rate(1);
-	while(ros::ok())
-	{
-		// publish a teleop command message
-		robot2017::Teleop command;
+    ros::Rate rate(1);
+    while(ros::ok())
+    {
+        // publish a teleop command message
+        robot2017::Teleop command;
 
-		command.a = randomBool();
-		command.b = randomBool();
-		command.x = randomBool();
-		command.y = randomBool();
-		command.lb = randomBool();
-		command.rb = randomBool();
-		command.back = randomBool();
-		command.start = randomBool();
-		command.l_thumb = randomBool();
-		command.r_thumb = randomBool();
-		command.x_l_thumb = rand();
-		command.y_l_thumb = rand();
-		command.x_r_thumb = rand();
-		command.y_r_thumb = rand();
-		command.l_trig = rand();
-		command.y_trig = rand();
+        command.a = randomBool();
+        command.b = randomBool();
+        command.x = randomBool();
+        command.y = randomBool();
+        command.lb = randomBool();
+        command.rb = randomBool();
+        command.back = randomBool();
+        command.start = randomBool();
+        command.l_thumb = randomBool();
+        command.r_thumb = randomBool();
+        command.x_l_thumb = rand();
+        command.y_l_thumb = rand();
+        command.x_r_thumb = rand();
+        command.y_r_thumb = rand();
+        command.l_trig = rand();
+        command.y_trig = rand();
 
-		teleopPub.publish(command);
-		rate.sleep();
-	}
+        teleopPub.publish(command);
+        rate.sleep();
+    }
 
-	return 0;
+    return 0;
 }

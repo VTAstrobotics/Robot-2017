5d20ff61556fbf39028b0644616a288b9b315e31
mcconnerVT <mcconner@vt.edu>
2017-02-04 13:05:35 -0500

    Updated test controller to test several random robot states

--- src/robot2017/src/test_controller.cpp
+++ src/robot2017/src/test_controller.cpp
@@ -21,9 +21,12 @@ int main(int argc, char **argv)
 
     // create a topic which will contain motor speed
     ros::Publisher teleopPub = nh.advertise<robot2017::Teleop>("/robot/teleop", 1000);
-
+    int count = 0;
     while(ros::ok())
     {
+        ros::Duration(2).sleep(); //sleep for 2 sec between messages
+
+
         // publish a teleop command message
         robot2017::Teleop command;
 
@@ -31,21 +34,56 @@ int main(int argc, char **argv)
         command.b = randomBool();
         command.x = randomBool();
         command.y = randomBool();
-        command.lb = randomBool();
+        //command.lb = randomBool();
         command.rb = randomBool();
         command.back = randomBool();
         command.start = randomBool();
         command.l_thumb = randomBool();
         command.r_thumb = randomBool();
         command.x_l_thumb = rand();
-        command.y_l_thumb = rand();
-        command.x_r_thumb = rand();
+        //command.y_l_thumb = rand();
+        //command.x_r_thumb = rand();
         command.y_r_thumb = rand();
-        command.l_trig = rand();
-        command.r_trig = rand();
+        //command.l_trig = rand();
+        //command.r_trig = rand();
 
+        if (count % 4 == 0) //State 1: deadman switch off (dead)
+        {
+            command.lb = 0;
+            command.y_l_thumb = rand();
+            command.x_r_thumb = rand();
+            command.l_trig = rand();
+            command.r_trig = rand();
+        }
+        else
+        {
+            command.lb = 1;
+            if (count % 4 == 1) //State 2: driving
+            {
+                command.y_l_thumb = rand() + 1;
+                command.x_r_thumb = rand() + 1;
+                command.l_trig = rand();
+                command.r_trig = rand();
+            }
+            else
+            {
+                command.y_l_thumb = 0;
+                command.x_r_thumb = 0;
+                if (count % 4 == 2) //State 3: Digging
+                {
+                    command.r_trig = (rand() % 99) + 1;
+                    command.l_trig = 0;
+                }
+                else //State 4: Dumping
+                {
+                    command.r_trig = 0;
+                    command.l_trig = (rand() % 99) + 1;
+
+                }
+            }
+        }
+        count++;
         teleopPub.publish(command);
-        ros::Duration(rand() % 5).sleep();
     }
 
     return 0;

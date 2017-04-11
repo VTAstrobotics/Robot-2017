cecd71ab470b483640b20d5d1a0a1b16351990e4
mcconnerVT <mcconner@vt.edu>
2017-04-10 22:01:41 -0400

    Added watchdog sub and pub

--- src/robot2017/src/watchdog_node.cpp
+++ src/robot2017/src/watchdog_node.cpp
@@ -5,11 +5,30 @@
 #include <ros/ros.h>
 #include <std_msgs/Int64.h>
 #include <string>
+#include "robot_msgs/Watchdog.h"
+
+robot_msgs::Watchdog wd;
+
+void watchdogReceived(const robot_msgs::Watchdog& msg)
+{
+    wd = msg;
+}
 
 int main(int argc, char **argv)
 {
     ros::init(argc, argv, "watchdog_node");
 
+    ros::NodeHandle nh;
+
+    ros::Subscriber sub = nh.subscribe("/serial/watchdog", 100, &watchdogReceived);
+    ros::Publisher pub = nh.advertise<robot_msgs::Watchdog>("/robot/watchdog", 100);
+
+    while (ros::ok())
+    {
+        ros::spinOnce();
+
+        pub.publish(wd);
+    }
 
     return 0;
 }
